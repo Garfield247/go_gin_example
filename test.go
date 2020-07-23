50a635a5b95756f30f42e0318b7d80b7d993d241
CatMan <garfield_lv@163.com>
2020-07-23 15:21:29 +0800

    create project

---
+++ test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context){
+        c.JSON(200,gin.H{
+            "message":"PONG",
+        })
+    })
+    r.GET("/", func(c *gin.Context){
+        c.JSON(200,gin.H{
+            "message":"go-gin-test",
+            "data":"HELLO GIN!",
+        })
+    })
+    r.Run()
+}
+
